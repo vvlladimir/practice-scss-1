031b29c1b9200d10cb9a046607451b2465c90942
Vladimir Todorovic <vvlladimir@gmail.com>
2017-04-13 12:58:40 +0200

    learning javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+//javascript file
+
+document.getElementById('dugme').addEventListener('click', function(e){
+  e.toElement.innerText = "Vlada";
+  console.log(e.toElement.innerText);
+}, false);
+
+
+
+var fotka = document.querySelectorAll('.wrapper > img');
+
+var boja = ['yellow', 'lightyellow', 'greenyellow' , 'magenta', 'darkblue', 'lightblue', 'blue', 'green', 'purple', 'black', 'grey'];
+
+for (var i = 0; i < fotka.length; i++) {
+
+  fotka[i].style.backgroundColor = boja[i];
+
+}
+
+
+
+
+document.querySelector('.wrapper').addEventListener('click', function(a){
+var bgColor = a.toElement.style.backgroundColor;
+//console.log(a.toElement.style.backgroundColor);
+
+  var removeTarget = a.target;
+//console.dir(removeTarget);
+  removeTarget.parentNode.removeChild(removeTarget);
+
+var boxPoruka = document.createElement('div');
+boxPoruka.className += 'box-poruka';
+
+var textPoruka = document.createTextNode('Hvala vam sto ste obrisali '+ bgColor +' kockicu!');
+
+boxPoruka.appendChild(textPoruka);
+document.getElementById('body').appendChild(boxPoruka);
+
+}, false);
